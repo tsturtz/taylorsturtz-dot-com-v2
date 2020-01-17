01644c516dc38b5e6e48fcde645f6af24a77f2f2
Taylor Sturtz <tsturtz@restaurant365.com>
2020-01-17 10:18:09 -0800

    added zoolander gif

--- src/pages/gifs.js
+++ src/pages/gifs.js
@@ -39,6 +39,7 @@ const gifUrls = [
   { alt: 'Excited girl', url: 'https://i.giphy.com/media/xTiN0CNHgoRf1Ha7CM/giphy-downsized.gif' },
   { alt: 'Emma Watson weird', url: 'https://media2.giphy.com/media/PEglcwKOSAjlu/giphy.gif' },
   { alt: 'Word!', url: 'https://i.giphy.com/media/xT1R9ERHwyzbCkIwla/giphy-downsized.gif' },
+  { alt: 'Zoonlander - you guyz!', url: 'https://media1.giphy.com/media/bXveYFUaYr67e/giphy.gif' },
 ]
 
 class GifsPage extends Component {
